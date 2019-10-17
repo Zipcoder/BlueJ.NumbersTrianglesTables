37dfcaf74621ec9087c03c69c923e5d8015015b5
Ron Duwell <ronduwell@zipcoders-MacBook-Pro.local>
2019-10-17 19:35:38 -0400

    Finished the Number Utilities

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,34 @@
- 
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= tableSize; i++){
+            for(int j = 1; j <= tableSize;j++){
+                if(i < 10){
+                    sb.append("  " + (i * j) + " |");
+                } else if (10 <= (i * j) && (i * j) < 100){
+                    sb.append(" " + i + " |");
+                } else {
+                    sb.append((i * j) + " |");
+                }
+            /*    if(j == tableSize){
+                    sb.append("  " + i * j + " |");
+                } else if(j == 1){
+                    sb.append("  " + i * j + " |");
+            } else {
+                sb.append(" " + i * j + " | ");
+            }*/
+        }
+        sb.append("\n");
     }
+    return sb.toString();
 }
+}    
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -1,21 +1,33 @@
- 
 
 public class TriangleUtilities {
+    
+    public static String getRow(Integer numberOfStars) {
+        String row = "";
+        for(int i = 1; i <= numberOfStars; i++){
+            row += "*";
+        }
+        return row;
+    }
 
-    public static String getRow(int numberOfStars) {
-        return null;
+    public static String getTriangle(int numberOfRows) {
+        String triangle = "";
+        for(int i = 1; i <= numberOfRows; i++){
+            triangle += getRow(i);
+            triangle += "\n";
+        }
+        return triangle;
+    }
+    public static String getSmallTriangle() {
+        return getTriangle(4);
     }
     
-    public static String getTriangle(int numberOfRows) {
-        return null;
-    }
-
-
-    public static String getSmallTriangle() {
-        return null;
-    }
-
     public static String getLargeTriangle() {
-        return null;
-    }
+        return getTriangle(9);
+    }  
 }
+
+    
+
+    
+
+
