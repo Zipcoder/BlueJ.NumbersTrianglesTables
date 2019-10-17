ec68280339285d6fb13c01f204c47d5db10fa368
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-10-17 19:34:32 -0400

    Finished NumberUtilities

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,11 +1,7 @@
 
-
-
 public class NumberUtilities {
-
-    public static String result = "";
-    
     public static String getRange(int stop) {
+        String result = "";
         for(int i = 0; i < stop; i++) {
             result = result + Integer.toString(i);
         }
@@ -13,28 +9,49 @@ public class NumberUtilities {
     }
 
     public static String getRange(int start, int stop) {
-        for(int i = 0; start < stop; i++) {
-            result = result + Integer.toString(start);
+        String result = "";
+        for(int i = start; i < stop; i++) {
+            result = result + Integer.toString(i);
         }
         return result;
     }
 
-
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++) {
+            if (i % step == 0) {
+                result = result + Integer.toString(i);
+            }
+        }
+        return result;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                result = result + Integer.toString(i);
+            }
+        }
+        return result;
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++) {
+            if (i % 2 == 1) {
+                result = result + Integer.toString(i);
+            }
+        }
+        return result;
     }
 
-
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String result = "";
+        for(int i = start; i <= stop; i++) {
+            int num = (int) Math.pow(i, exponent);
+            result = result + Integer.toString(num);
+        }
+        return result;
     }
 }
