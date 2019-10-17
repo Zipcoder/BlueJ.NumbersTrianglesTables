19d060be7b75879978aa9b6bffdb5f975c28eef3
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-17 19:26:04 -0400

    added logic for triangles

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,53 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String rowStars = "";
+        
+        for (int i=0; i<numberOfStars; i++){
+            rowStars += "*"; 
+        }    
+        return rowStars;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String row = "";
+        
+        for (int i=1; i<numberOfRows + 1; i++){
+            
+            for (int j=0; j<i; j++){
+               row += "*";   
+            }
+            row = row + "\n";
+        }    
+        return row;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String row = "";
+        int numberOfRows = 4;
+        
+        for (int i=1; i<numberOfRows + 1; i++){
+            
+            for (int j=0; j<i; j++){
+               row += "*";   
+            }
+            row = row + "\n";
+        }    
+        return row;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String row = "";
+        int numberOfRows = 9;
+        
+        for (int i=1; i<numberOfRows + 1; i++){
+            
+            for (int j=0; j<i; j++){
+               row += "*";   
+            }
+            row = row + "\n";
+        }    
+        return row;
     }
 }
