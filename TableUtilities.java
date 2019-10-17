dd7248da4794878e7de81dab0657289f1030d6ed
deltagphys <cty.jgates@gmail.com>
2019-10-16 21:35:26 -0400

    mult table class completed

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,21 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String response = "";
+        for (int i = 1; i <= tableSize; i++){
+            for (int j = 1; j <= tableSize; j++) {
+                response += new String(new char[3-Integer.toString(i*j).length()]).replace("\0", " ")+ i*j +" |";
+            }
+            response += "\n";
+        }
+        return response;
     }
 }
