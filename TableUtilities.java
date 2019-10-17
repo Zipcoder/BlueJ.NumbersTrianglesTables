86c9eed520c6885dc2f9ce79f0450ed8ea2b661d
Wes Jones <wes@MacBook-Pro-5.localdomain>
2019-10-17 18:26:03 -0400

    all tests passing for TableUtilitiesTest

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,47 @@
  
 
 public class TableUtilities {
+    public static String getSpacesBeforeNumbers(int numToCheck) {
+        int lengthOfNum = String.valueOf(numToCheck).length();
+        String spacer = "";
+        for (int i = 0; i < (3 - lengthOfNum); i++) {
+            spacer += " ";
+        }
+        return spacer;
+    }
     public static String getSmallMultiplicationTable() {
-        return null;
+        String acc = "";
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                int numVal = j * i;
+                acc += getSpacesBeforeNumbers(numVal) + String.valueOf(numVal) + " |";
+            }
+            acc += "\n";
+        }
+        return acc;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String acc = "";
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                int numVal = j * i;
+                acc += getSpacesBeforeNumbers(numVal) + String.valueOf(numVal) + " |";
+            }
+            acc += "\n";
+        }
+        return acc;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String acc = "";
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                int numVal = j * i;
+                acc += getSpacesBeforeNumbers(numVal) + String.valueOf(numVal) + " |";
+            }
+            acc += "\n";
+        }
+        return acc;
     }
 }
