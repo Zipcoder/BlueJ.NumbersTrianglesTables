fe2c9a686db7a88d3cf3f5d230c3fe9e7a01bbd2
Mike Shoenbeck <mike@zipcoders-MacBook-Pro-4.local>
2019-10-18 16:13:20 -0400

    Finished Lab

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,86 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+       String result = "";
+       
+        for(int i = 0; i < stop; i++)  {
+           String str = Integer.toString(i);
+           result = result + str;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+       
+        for(int i = start; i < stop; i++)  {
+           String str = Integer.toString(i);
+           result = result + str;
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        
+        
+        for(int i = start; i < stop; i++)  {
+            if(i % step != 0)  {
+                continue;
+            }   else    {
+                
+           String str = Integer.toString(i);
+           result = result + str;
+        }
+    }
+        return result;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+       
+        for(int i = start; i < stop; i++)  {
+            if(i % 2 != 0)  {
+                continue;
+            }   else    {
+                
+           String str = Integer.toString(i);
+           result = result + str;
+        }
+    }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+       
+        for(int i = start; i < stop; i++)  {
+            if(i % 2 == 0)  {
+                continue;
+            }   else    {
+                
+           String str = Integer.toString(i);
+           result = result + str;
+        }
+    }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String result = "";
+        int exp;
+        
+        for(int i = start; i < stop + 1; i++)  {
+           exp = (int) Math.pow(i,exponent);
+           
+     
+           String str = Integer.toString(exp);
+           result = result + str;
+        }return result;
     }
+        
+    
 }
