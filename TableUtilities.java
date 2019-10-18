2a2150d7a604fd181ca3af75c12e287fe51f78c1
Ron Duwell <ronduwell@zipcoders-MacBook-Pro.local>
2019-10-17 20:07:13 -0400

    Finished Tables Utilities

--- TableUtilities.java
+++ TableUtilities.java
@@ -12,20 +12,17 @@ public class TableUtilities {
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= tableSize; i++){
             for(int j = 1; j <= tableSize;j++){
-                if(i < 10){
+                int product = (i * j);
+                sb.append(String.format("%3d |",product));
+                
+                /*if(i < 10){
                     sb.append("  " + (i * j) + " |");
-                } else if (10 <= (i * j) && (i * j) < 100){
-                    sb.append(" " + i + " |");
+                } else if (10 <= j && j < 100){
+                    sb.append(" " + (i * j) + " |");
                 } else {
                     sb.append((i * j) + " |");
-                }
-            /*    if(j == tableSize){
-                    sb.append("  " + i * j + " |");
-                } else if(j == 1){
-                    sb.append("  " + i * j + " |");
-            } else {
-                sb.append(" " + i * j + " | ");
-            }*/
+                }*/
+
         }
         sb.append("\n");
     }
