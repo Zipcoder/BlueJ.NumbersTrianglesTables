143d6be972a4eb15f25a7c8ee31ceac96d7e2646
Miral Doshi <miraldoshi@Zipoders-MacBook-Pro-2.local>
2019-10-18 17:54:01 -0400

    Please enter the commit message for your changes. Lines starting

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,62 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String rangeTo = "";
+        for(int i=0; i< stop; i++ ){
+            rangeTo += i;
+        }
+         
+        return rangeTo;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String rangeTo = "";
+        for(int i = start; i < stop; i++){
+            rangeTo += i;
+        }
+        return rangeTo;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String rangeTo = "";
+        for(int i = start; i < stop; i += step){
+            rangeTo += i;
+        }
+        return rangeTo;
+        
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String rangeTo = "";
+        for(int i = start; i < stop; i++){
+           if (i % 2 == 0){
+            rangeTo += i;
+        }
+        }
+        return rangeTo;
+  
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String rangeTo = "";
+        for(int i = start; i < stop; i++){
+           if (i % 2 != 0){
+            rangeTo += i;
+        }
+        }
+        return rangeTo;
+  
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String rangeTo = "";
+        for(int i = start; i <= stop; i++){
+            rangeTo += (int)Math.pow(i, exponent); 
+            
+        }
+        return rangeTo;
     }
 }
