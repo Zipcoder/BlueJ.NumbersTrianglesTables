b60497afae5cd14319701c762bc7d1c4e75c9818
Val Fragier <val@zipcoders-MacBook-Pro-8.local>
2019-10-17 20:04:31 -0400

    solved tables

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,31 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String outcome = "";
+        for(int i = 1; i <= tableSize; i++){
+            for(int y = 1; y <= tableSize; y++) {
+                if(i * y < 10){
+                    outcome += "  " + i * y + " |";
+                }
+                else if(i * y < 100){
+                    outcome += " " + i * y + " |";
+                }
+                else{
+                    outcome += "" + i * y + " |";
+                }
+            }
+            outcome += "\n";
+        }
+       System.out.println(outcome);
+       return outcome;
     }
 }
