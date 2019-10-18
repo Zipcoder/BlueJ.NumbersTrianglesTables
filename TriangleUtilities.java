eeca46078a0c82d97536b56137faae7d721c155d
MannyMonocle <eetumadu@gmail.com>
2019-10-18 08:31:35 -0400

    Halfway through triangles

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,11 +3,22 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String ans = "";
+        for(int i=0;i<numberOfStars;i++){
+           ans +="*";
+        }
+        return ans;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String ans = "";
+        for(int i=1;i<=numberOfRows;i++){
+           for(int j=1;j<=i;j++){
+               if(j!=i){ans += "*";}
+               else {ans += "*\n";}
+            }
+        }
+        return ans;
     }
 
 
