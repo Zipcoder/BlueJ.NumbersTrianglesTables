4e62e5ab66fe5bc5973cf11cbced7ba1569f74c3
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-18 16:07:31 -0400

    logic for tables

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,27 @@
  
 
 public class TableUtilities {
+        public static String getMultiplicationTable(int tableSize) {
+        
+        String tableString = "";
+        
+        for(int i=1; i<=tableSize; i++){ 
+            for(int j=1; j<=tableSize; j++){
+                
+                tableString += String.format("%3d", i*j) + " |";
+            }
+            tableString += "\n";
+        }
+        return tableString;
+    }
+
+    
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
-    public static String getMultiplicationTable(int tableSize) {
-        return null;
-    }
 }
