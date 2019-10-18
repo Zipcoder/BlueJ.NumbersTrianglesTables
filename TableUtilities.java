1a2b949347cc777d81310a7ce08df739074d6d23
Ryan Miller <ryan@zipcoders-MacBook-Pro-10.local>
2019-10-18 12:55:14 -0400

    loops within loops

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,24 @@
- 
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table = "";
+        for(int i = 1; i <= tableSize; i++) {
+            for(int j = 1; j <= tableSize; j++) {
+                if(i * j < 10)  table += " "; 
+                if(i * j < 100) table += " ";
+                table += i*j + " |";
+            }
+            table += "\n";
+        }
+        System.out.println(table);
+        return table;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,24 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        return "*".repeat(numberOfStars);
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        for(int i = 1; i <= numberOfRows; i++) {
+            triangle += getRow(i) + "\n";
+        }
+        System.out.println(triangle);
+        return triangle;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
