aace873b238c3d1ba07586bb6761840b148db408
MannyMonocle <eetumadu@gmail.com>
2019-10-17 22:10:20 -0400

    Numbers working

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,61 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String ans = "";
+        for(int i=0;i<stop;i++){
+           ans +=i;
+        }
+        return ans;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String ans = "";
+        for(int i=start;i<stop;i++){
+           ans +=i;
+        }
+        return ans;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+         String ans = "";
+        for(int i=start;i<stop;i=i+step){
+           ans +=i;
+        }
+        return ans;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+           String ans = "";
+        for(int i=start;i<stop;i++){
+           if(i%2==0) {
+            ans +=i;
+        } else {
+            ans = ans;
+        }
+        }
+        return ans;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+           String ans = "";
+        for(int i=start;i<stop;i++){
+           if(i%2==1) {
+            ans +=i;
+        } else {
+            ans = ans;
+        }
+        }
+        return ans;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String ans = "";
+        for(int i=start;i<=stop;i++){
+           ans +=(int)Math.pow(i,exponent);
+        }
+        return ans;
     }
 }
