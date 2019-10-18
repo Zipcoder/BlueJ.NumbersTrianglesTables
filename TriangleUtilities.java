46709a5f07a5dd130f4398f475de089d4b09a59b
Kane Kopetski <kane@zipcoders-MacBook-Pro-8.local>
2019-10-17 22:00:20 -0400

    Finished triangles method.

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -1,21 +1,40 @@
- 
 
 public class TriangleUtilities {
-
     public static String getRow(int numberOfStars) {
-        return null;
-    }
-    
-    public static String getTriangle(int numberOfRows) {
-        return null;
+        String string = "";
+        for(int i = 1; i <= numberOfStars; i++){
+            string = string + "*";
+        }
+        return string;
     }
 
+    public static String getTriangle(int numberOfRows) {
+        String string = "";
+        for (int i = 1; i <= numberOfRows; i++)
+        {  
+            for (int j = 1; j <= i; j++)
+            string = string + "*";
+            string = string + "\n";
+        }   return string;
+    } 
 
     public static String getSmallTriangle() {
-        return null;
+          String string = "";
+        for (int i = 1; i <= 4; i++)
+        {  
+            for (int j = 1; j <= i; j++)
+            string = string + "*";
+            string = string + "\n";
+        }   return string;
     }
 
     public static String getLargeTriangle() {
-        return null;
+              String string = "";
+        for (int i = 1; i <= 9; i++)
+        {  
+            for (int j = 1; j <= i; j++)
+            string = string + "*";
+            string = string + "\n";
+        }   return string;
     }
 }
