8a074a1093926384ca3b25217ecd0048118030bb
Kievina <56592501+Kievina@users.noreply.github.com>
2019-10-19 12:58:13 -0400

    Completed method stubs in the class

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,65 @@
 
-
-
-public class NumberUtilities {
-
+public class NumberUtilities { 
     public static String getRange(int stop) {
-        return null;
+        String range = "";
+        for(int i = 0; i < stop; i++){
+            String nextNum = Integer.toString(i);
+            range += nextNum;
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "";
+        for(int i = start; i < stop; i++){
+            String nextNum = Integer.toString(i);
+            range += nextNum;
+        }
+        return range;
     }
 
-
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for(int i = start; i < stop; i+=step) {
+            String nextNum = Integer.toString(i);
+            range += nextNum;
+        }
+        return range;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evenNums = "";
+        if(start % 2 != 0) {
+            start += 1;
+        }
+        for(int i = start; i < stop; i += 2) {
+            String nextNum = Integer.toString(i);
+            evenNums += nextNum; 
+        }
+        return evenNums;
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String oddNums = "";
+        if(start % 2 == 0) {
+            start += 1;
+        }
+        for(int i = start; i < stop; i += 2) {
+            String nextNum = Integer.toString(i);
+            oddNums += nextNum; 
+        }
+        return oddNums;
     }
-
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String expNumsStr = "";
+        String nextNumStr;
+        for(int i = start; i <= stop; i++) {
+            
+            nextNumStr = Integer.toString((int) Math.pow(i,exponent));
+            expNumsStr += nextNumStr;
+        }
+        return expNumsStr;
     }
 }
+
