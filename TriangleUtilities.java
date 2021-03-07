0a8629cd050c773875e70d8ee3753227272e2243
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-07 14:29:28 -0500

    tests passing on numbers. Not triangles or table  Please enter the commit message for your changes. Lines starting

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,12 +3,40 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String output = "";
+        char[] starArr = new char[numberOfStars];
+        for (int i = 0; i < numberOfStars; i++) {
+            starArr[i] = '*';
+            String strStars = Character.toString(starArr[i]);
+            output += strStars;
+        }
+        return output;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
-    }
+         int i, j;
+         String output = "";
+        // outer loop to handle number of rows
+        //  n in this case
+        for(i=0; i<=numberOfRows; i++)
+        { String holder = "";
+ 
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop    
+            for(j=0; j<i; j++)
+            {
+                // printing stars
+                holder += getRow(i) + "\n";
+            }
+ 
+            // ending line after each row
+            //System.out.println();
+            output += holder;
+        }
+        System.out.print(output);
+        return output;
+    } 
+
 
 
     public static String getSmallTriangle() {
