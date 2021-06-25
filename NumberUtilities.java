06ecb770f1ffc62268322eebfc2d48d8abbf5fc3
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-06-25 18:04:27 -0400

    Code passed all tests

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,63 @@
 
-
+import java.lang.Math;
 
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String x = "";
+        for (int i = 0; i < stop; i++){
+            x += i;
+        }
+        return x;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String x = "";
+        for (int i = start; i < stop; i++){
+            x += i;
+        }
+        return x;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String x = "";
+        int i = start;
+        while (i < stop) {
+            x += i;
+            i += step;
+        }
+        return x;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String x = "";
+        for (int i = start; i < stop; i++){
+            if((i % 2) == 0){
+                x += i;
+            }
+        }
+        return x;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String x = "";
+        for (int i = start; i < stop; i++){
+            if((i % 2) == 1){
+                x += i;
+            }
+        }
+        return x;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String x = "";
+        for (int i = start; i <= stop; i++){
+            int a = (int)Math.pow(i,exponent);
+            x += a;
+        }
+        return x;
     }
 }
