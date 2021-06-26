e9a1e736e4d1e2f6e05fcd56bcfd9feff53b6d69
Raymond <raymond@zipcoder.local>
2021-06-26 00:55:20 -0400

    Raymond Fitzgerald

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,58 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+       String rangeString = "";
+       for (int i = 0; i < stop; i++){
+           rangeString += i;
+        }
+        return rangeString;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String rangeString = "";
+       for (int i = start; i < stop; i++){
+           rangeString += i;
+        }
+        return rangeString;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String rangeString = "";
+       for (int i = start; i < stop; i+=step){
+           rangeString += i;
+        }
+        return rangeString;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evenNumbers = "";
+        for (int i = start; i < stop; i++){
+            if (i % 2 == 0){
+                evenNumbers +=i;
+            }
+        }
+        return evenNumbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+          String oddNumbers = "";
+        for (int i = start; i < stop; i++){
+            if (i % 2 == 1){
+                oddNumbers +=i;
+            }
+        }
+        return oddNumbers;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String exNumbers = "";
+        for (int i = start; i <= stop; i++){
+           exNumbers += (int)Math.pow(i,exponent);
+          }
+        
+        return exNumbers;
     }
 }
--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,29 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
+        String multTable = "";
+        for (int i = 1; i <=5; i++){
+             for (int j = 1; j <= 5; j++){
+                multTable += String.format("%3d |", i*j);
+    } multTable += "\n";
+}return multTable;
+}
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String multTable = "";
+        for (int i = 1; i <=10; i++){
+             for (int j = 1; j <= 10; j++){
+                multTable += String.format("%3d |", i*j);
+    } multTable += "\n";
+}return multTable;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String multTable = "";
+        for (int i = 1; i <=tableSize; i++){
+             for (int j = 1; j <= tableSize; j++){
+                multTable += String.format("%3d |", i*j);
+    } multTable += "\n";
+}return multTable;
     }
 }
