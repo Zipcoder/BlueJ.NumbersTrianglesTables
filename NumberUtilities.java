d3a0faa6119ba6245536705b4781946589287ffe
Manny <manny@ZCW-JLAT.home>
2021-06-26 00:18:10 -0400

    Manny finished NumbersTrianglesTables

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -2,31 +2,65 @@
 
 
 public class NumberUtilities {
+    
 
     public static String getRange(int stop) {
-        return null;
-    }
+        String result = "";
+        for(int i = 0; i < stop; i++) {
+            result += i;
+        }
+        return result;
+}
 
     public static String getRange(int start, int stop) {
-        return null;
-    }
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            result += i;
+        }
+        return result;
+}
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
-    }
+        String result = "";
+        for (int i = start; i < stop; i += step) {
+            result += i;
+        }
+        return result;
+}
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if ( i % 2 == 0) {
+                result += i;
+        }
+         
     }
+    return result;
+}
+
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if ( i % 2 != 0) {
+                result += i;
+        }
+        
     }
+    return result;
+}
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
-    }
+        String result = "";
+        for (int i = start; i <= stop; i++) {
+            result += (int)Math.pow(i, exponent);
+        }
+        System.out.print(result);
+        return result;
+}
+    
 }
