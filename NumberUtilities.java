623be5f8ce8f4ad7980c531e522eb60283f68fe2
johnS93 <john.song8293@gmail.com>
2021-06-26 01:26:46 -0400

    here is my changes

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,59 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String x = "";
+        for(int i = 0; i<stop; i++){
+            x += i;
+        }
+        return x;
     }
+    
 
     public static String getRange(int start, int stop) {
-        return null;
+       String x = "";
+        for(int i = start; i<stop; i++){
+            x += i;
+        }
+        return x;
     }
-
+    
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String x = "";
+        for(int i = start; i<stop; i+=step){
+            x += i;
+        }
+        return x;
     }
+    
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String x = "";
+        for(int i = start; i<stop; i++){
+            if (i % 2 == 0) {
+                x += i;
+            }
+        }
+        return x;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String x = "";
+        for(int i = start; i<stop; i++){
+            if (i % 2 != 0) {
+                x += i;
+            }
+        }
+        return x;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String x = "";
+        for(int i = start; i<=stop; i++){
+            x += (int)Math.pow(i,exponent);
+        }
+        return x;
     }
 }
