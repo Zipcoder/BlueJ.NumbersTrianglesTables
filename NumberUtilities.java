724cb406e7cd53c5d082a715d150df893cb7fe6d
Jeremy <jeremysabina@gmail.com>
2021-06-25 20:39:58 -0400

    My brain hurts and it has now turned into goo

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,60 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
-    }
+        String outcome = "";
+        for(int i = 0; i < stop; i++){
+            outcome += i;
+        } return outcome;
+    } 
 
     public static String getRange(int start, int stop) {
-        return null;
+        String outcome = "";
+        for(int i = start; i < stop; i++){
+            outcome += i;
+        } return outcome;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+            String outcome = "";
+        for(int i = start; i < stop; i+=step){
+            outcome += i;
+        } return outcome;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
-    }
+             String outcome = "";
+             int j = start % 2;
+        
+             
+             if (j != 0){  
+        for(int i = start + 1; i < stop; i+=2){
+            outcome += i;
+        } return outcome;
+    } else {
+        for(int i = start; i < stop; i+=2){
+            outcome += i;
+        }
+       return outcome; 
+    } 
+}
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+            String outcome = "";
+        for(int i = start; i < stop; i+=2){
+            if (i%2 == 1){
+                outcome += i;
+                }
+                }return outcome;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+                String outcome = "";
+
+        for(int i = start; i <= stop; i++){
+            outcome = outcome + (int)Math.pow(i, exponent);
+        } return outcome;
     }
 }
