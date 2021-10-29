fec1c2af5206d6b49d3232f21c05681007079a8b
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-10-28 23:52:09 -0400

    completed challenging lab

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,80 @@
-
+import java.util.ArrayList;  
+import java.util.Arrays;  
 
 
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < stop; i++){
+           sb.append(i);
+        }
+        
+        return sb.toString();
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = start; i < stop; i++){
+           sb.append(i);
+        }
+        
+        return sb.toString();
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = start; i < stop; i+=step){
+           sb.append(i);
+        }
+        
+        return sb.toString();
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+         StringBuilder sb = new StringBuilder();
+        
+        for(int i = start; i < stop; i++){
+           if(i % 2 == 0){
+            sb.append(i);
+            } 
+           
+        }
+        
+        return sb.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = start; i < stop; i++){
+           if(i % 2 == 1){
+            sb.append(i);
+            } 
+           
+        }
+        
+        return sb.toString();
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+       
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = start; i <= stop; i++){
+         
+            sb.append((int)Math.pow(i,exponent));
+            
+           
+        }
+        
+        return sb.toString();
     }
 }
