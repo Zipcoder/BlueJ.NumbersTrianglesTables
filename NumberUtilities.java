0d8a07b88f99f7a51f4bf9df0b7518fa9ec9e347
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-10-30 00:59:04 -0400

    lab complete

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,67 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        return getRange(0, stop);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String numberLine = "";
+        for (int i = start; i < stop; i++) {
+            numberLine += i;
+
+        }
+        return numberLine;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String stepNumber = "";
+        for (int i = start; i < stop; i += step) {
+             stepNumber += i;
+
+        }
+        return stepNumber;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evenNumbers = "";
+        for (int i = start; i < stop; i++) {
+             //is i even?
+            //if it is even then then record that number in evenNumbers
+            if (i%2 == 0) {
+                evenNumbers += i;
+            }
+
+        }
+        return evenNumbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+            //asking for a string of all of the odd numbers between start and stop, noninclusive of stop
+        String oddNumbers = "";
+        for (int i = start; i < stop; i++) {
+             if (i%2 == 1) {
+                 oddNumbers += i;
+             }
+
+        }
+        return oddNumbers;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        //Math.pow(,)
+        String expoNumbers = "";
+        for (int i = start; i <= stop; i++) {
+             //raise to a power
+            double num = Math.pow(i,exponent);
+            //casts to an int
+            int numOne = (int) num;
+            //add it to the string
+            expoNumbers += numOne;
+        }
+
+        return expoNumbers;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,42 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+
+        for (int i = 0; i < numberOfStars; i++) {
+            // return a row of stars
+            //add to row a star
+            row = row + "*";
+
+        }
+        return row;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        //outter loop as vertical
+        //inside loop as backwards to vertical
+        String row = "";
+        for (int i = 1; i <= numberOfRows; i++) {
+
+            for (int j = i; j > 0; j--) {
+                row = row + "*";
+
+            }
+            row = row + "\n";
+
+        }
+
+        return row;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+
+        return getTriangle(9);
     }
 }
