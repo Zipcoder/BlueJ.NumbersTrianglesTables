bb51d577c5ab284bd5c23d002c74f128a59951cd
Ellis John <ellisjohn@zipcodeMBPchuck.fios-router.home>
2021-10-30 22:35:49 -0400

    completed lab

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,62 @@
  
 
 public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
-        return null;
+public static String getSmallMultiplicationTable() {
+        String table = "";
+        
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=5; j++) {
+                if (j*i < 10) {
+              table = table + (("  " + (j * i)) + " |");   
+            } else {
+                table = table + ((" " + (j * i)) + " |");;
+            }
+             
+        }
+    table = table + "\n";
+}    
+        return table;
     }
 
+    
+    
     public static String getLargeMultiplicationTable() {
-        return null;
-    }
+        String table = "";
+        
+        for(int i = 1; i<=10; i++){
+            for(int j = 1; j<=10; j++) {
+                if (j*i < 10) {
+              table = table + (("  " + (j * i)) + " |");   
+            } else if(j*i < 100){
+                table = table + ((" " + (j * i)) + " |");
+            } else {
+                table = table + (("" + (j * i)) + " |");
+            }
+             
+        }
+    
+    table = table + "\n";
+}    
+    return table;
+}
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+                String table = "";
+        
+        for(int i = 1; i<=tableSize; i++){
+            for(int j = 1; j<=tableSize; j++) {
+                if (j*i < 10) {
+              table = table + (("  " + (j * i)) + " |");   
+            } else if(j*i < 100){
+                table = table + ((" " + (j * i)) + " |");
+            } else {
+                table = table + (("" + (j * i)) + " |");
+            }
+             
+        }
+    
+    table = table + "\n";
+}    
+    return table;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,48 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String stars = "";
+        for (int i = 1; i<=numberOfStars; i++){
+            stars = stars + "*";
+        }
+        return stars;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String stars = "";
+        
+        for(int i = 1; i<=numberOfRows; i++){
+            for(int j = 1; j<=i; j++) {
+              stars = stars + "*";   
+            }
+            stars = stars + "\n"; 
+        }
+        return stars;
+        
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String stars = "";
+        
+        for(int i = 1; i<=4; i++){
+            for(int j = 1; j<=i; j++) {
+              stars = stars + "*";   
+            }
+            stars = stars + "\n"; 
+        }
+        return stars;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String stars = "";
+        
+        for(int i = 1; i<=9; i++){
+            for(int j = 1; j<=i; j++) {
+              stars = stars + "*";   
+            }
+            stars = stars + "\n"; 
+        }
+        return stars;
     }
 }
