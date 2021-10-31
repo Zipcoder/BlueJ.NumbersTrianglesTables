e6337dbd6dcd07f6d23525800f1e06c54a63f4fb
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-10-31 03:51:10 -0400

    Completed lab

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,100 @@
- 
-
-public class TableUtilities {
+ public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String outcome = "";
+        String convert;
+        int multiple;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                multiple = i * j;
+                convert = Integer.toString(multiple);
+
+                if (multiple >= 10) {
+                    if (j == 5) {
+                        outcome += " " + convert + " |\n";
+                    } else {
+                        outcome += " " + convert + " |";
+                    }
+                } else {
+                    if (j == 5) {
+                        outcome += "  " + convert + " |\n";
+                    } else {
+                        outcome += "  " + convert + " |";
+                    }
+                }
+            }
+        }
+        
+        return outcome;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String outcome = "";
+        String convert;
+        int multiple;
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                multiple = i * j;
+                convert = Integer.toString(multiple);
+
+                if (multiple >= 100) {
+                    if (j == 10) {
+                        outcome += "" + convert + " |\n";
+                    } else {
+                        outcome += "" + convert + " |";
+                    }
+                } else if (multiple >= 10){
+                    if (j == 10) {
+                        outcome += " " + convert + " |\n";
+                    } else {
+                        outcome += " " + convert + " |";
+                    }
+                } else {
+                    if (j == 10) {
+                        outcome += "  " + convert + " |\n";
+                    } else {
+                        outcome += "  " + convert + " |";
+                    }
+                }
+            }
+        }
+        
+        return outcome;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String outcome = "";
+        String convert;
+        int multiple;
+
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                multiple = i * j;
+                convert = Integer.toString(multiple);
+
+                if (multiple >= 100) {
+                    if (j == tableSize) {
+                        outcome += "" + convert + " |\n";
+                    } else {
+                        outcome += "" + convert + " |";
+                    }
+                } else if (multiple >= 10){
+                    if (j == tableSize) {
+                        outcome += " " + convert + " |\n";
+                    } else {
+                        outcome += " " + convert + " |";
+                    }
+                } else {
+                    if (j == tableSize) {
+                        outcome += "  " + convert + " |\n";
+                    } else {
+                        outcome += "  " + convert + " |";
+                    }
+                }
+            }
+        }
+        
+        return outcome;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -1,21 +1,45 @@
- 
-
-public class TriangleUtilities {
+ public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String outcome = "";
+
+        for (int i = 0; i < numberOfStars; i++) {
+            outcome += "*";
+        }
+        
+        return outcome;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String outcome = "";
+        String stars = "*";
+
+        for (int i = 1; i <= numberOfRows; i++) {
+            outcome += stars.repeat(i) + "\n";
+        }
+        
+        return outcome;
     }
 
-
     public static String getSmallTriangle() {
-        return null;
+        String outcome = "";
+        String stars = "*";
+
+        for (int i = 1; i <= 4; i++) {
+            outcome += stars.repeat(i) + "\n";
+        }
+        
+        return outcome;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String outcome = "";
+        String stars = "*";
+
+        for (int i = 1; i <= 9; i++) {
+            outcome += stars.repeat(i) + "\n";
+        }
+        
+        return outcome;
     }
 }
