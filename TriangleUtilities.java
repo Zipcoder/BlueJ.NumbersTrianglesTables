d5edada1a89dc77a64dc149f4c65d329e51eab4b
c-poteat <nellp@mail.com>
2021-10-31 21:37:21 -0400

    I have completed the assignment

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,45 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+    String count = "";
+    for (int i = 0; i < numberOfStars; i++) {
+    count = count + "*"; 
+    }
+    return count;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
-    }
+        String count = "";
+        for (int i = 1; i <= numberOfRows; i++) {
+        for(int j = i; j > 0; j--) {
+            
+        count = count + "*";
+        }
+        count = count + "\n";
+     }
+     return count;
+     
+}
 
 
     public static String getSmallTriangle() {
-        return null;
-    }
+           String count = "";
+        for (int i = 0; i <= 4; i++) { 
+        for (int j = 1; j <= i; j++) {
+            // column is less than equal to row
+            count = count + "*";
+            }
+        count = count + "\n";
+        return getTriangle(4);
+         }
+         return count;
+        }
+      
+   
 
     public static String getLargeTriangle() {
-        return null;
+      return getTriangle(9);
     }
 }
+
+
