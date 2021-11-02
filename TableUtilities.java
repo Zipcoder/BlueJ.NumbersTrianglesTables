67894f98c170add3339827aaee8619e270c7782a
Ellis John <ellisjohn@zipcodes-MacBook-Pro-chuck.local>
2021-11-02 14:03:30 -0400

    message

--- TableUtilities.java
+++ TableUtilities.java
@@ -4,7 +4,7 @@ public class TableUtilities {
 public static String getSmallMultiplicationTable() {
         String table = "";
         
-        for(int i = 1; i<=5; i++){
+        for(int i = 1; i<=8; i++){
             for(int j = 1; j<=5; j++) {
                 if (j*i < 10) {
               table = table + (("  " + (j * i)) + " |");   
