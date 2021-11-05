c33816136a2739e61c08d318e493ee89d6ce24c2
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-05 15:01:25 -0400

    completed

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,25 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result ="";
+        for(int i = 1; i <= tableSize; i++){
+        for(int k = 1; k <= tableSize; k++){
+         result += String.format("%3s", i*k) + " |";   
+            
+        
+        }
+        
+        result += "\n";
+        }
+              
+        return result;
     }
 }
